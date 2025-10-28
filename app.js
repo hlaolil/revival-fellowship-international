65b2f9db4ea0d8f7a4bf6fab1a70b6ba05a3664b
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-28 07:05:47 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// app.js - Main server file
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+// Controllers
+const homeController = require('./controllers/homeController');
+const aboutController = require('./controllers/aboutController');
+const ministriesController = require('./controllers/ministriesController');
+const joinController = require('./controllers/joinController');
+
+// Routes
+app.get('/', homeController.index);
+app.get('/about', aboutController.index);
+app.get('/ministries', ministriesController.index);
+app.get('/join', joinController.index);
+app.post('/join', joinController.submit);
+app.get('/thankyou', joinController.thankyou);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
