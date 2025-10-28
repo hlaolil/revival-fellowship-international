9f03bcec1b2cc3fd6e2f09577d19c38fb91bab14
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-28 08:32:53 +0200

    Update joinController.js

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,17 +1,15 @@
 // controllers/joinController.js
-
 exports.index = (req, res) => {
   res.render('join', {
     title: 'Join RFI',
     currentYear: new Date().getFullYear(),
-    lastModified: new Date().toLocaleString(), // FIXED: Server-side date
+    lastModified: new Date().toLocaleString(),
     activePage: 'join'
   });
 };
 
 exports.submit = (req, res) => {
-  const formData = req.body;
-  console.log('Form submitted:', formData);
+  console.log('Form data:', req.body);
   res.render('join-success', {
     title: 'Thank You',
     currentYear: new Date().getFullYear(),
