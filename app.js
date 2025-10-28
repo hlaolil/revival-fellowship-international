ba62da5ed20b56addb3c70ca62d2ce815adb054b
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-28 08:42:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const ministriesController = require('./controllers/ministriesController');
 const joinController = require('./controllers/joinController');
-const joinController = require('./controllers/joinController');   // <-- ADD THIS
 
 // Routes
 app.get('/', homeController.index);
