cd81e9012a00f730e1a43e76f7d47d2805ddaa82
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-28 14:15:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
 // app.js
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 // Import Controllers
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const ministriesController = require('./controllers/ministriesController');
-const joinController = require('./controllers/joinController'); // Must exist!
+const joinController = require('./controllers/joinController'); // ← now safe
 
-// View engine setup
+// View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
 
-// === ROUTES ===
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+// Routes
 app.get('/', homeController.index);
 app.get('/about', aboutController.index);
 app.get('/ministries', ministriesController.index);
-app.get('/join', joinController.index);     // This was broken
-app.post('/join', joinController.submit);   // Optional: form handler
+app.get('/join', joinController.index);
+app.post('/join', joinController.submit);
 
-// 404 Page
+// 404
 app.use((req, res) => {
-  res.status(404).render('404', { 
+  res.status(404).render('404', {
     title: 'Page Not Found',
     currentYear: new Date().getFullYear()
   });
 });
 
-// Error Handler
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
