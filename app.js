38e81bbbedb992249a35ac4de2358adff418e2e0
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-29 07:30:57 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 // app.js
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Import Controllers
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const ministriesController = require('./controllers/ministriesController');
-const joinController = require('./controllers/joinController'); // ← now safe
+const joinController = require('./controllers/joinController');
 
 // View engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', homeController.index);
@@ -23,13 +21,24 @@ app.get('/about', aboutController.index);
 app.get('/ministries', ministriesController.index);
 app.get('/join', joinController.index);
 app.get('/inquire', joinController.inquire);
-app.post('/inquire', joinController.submit);
+app.post('/inquire', joinController.submitInquire); // ← fix if needed
 
-// 404
-app.use((req, res) => {
-  res.status(404).render('404', {
+// 404 Page - MUST come AFTER all routes
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('404', {
     title: 'Page Not Found',
-    currentYear: new Date().getFullYear()
+    currentYear: new Date().getFullYear(),
+    activePage: ''
+  }, (err, html) => {
+    if (err) {
+      res.send(`
+        <h1>404 - Not Found</h1>
+        <p><a href="/">Go Home</a></p>
+      `);
+    } else {
+      res.send(html);
+    }
   });
 });
 
