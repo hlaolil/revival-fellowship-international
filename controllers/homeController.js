f8d91f79e8984baf0abe5183fe87bc808b2af6f6
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-29 13:16:20 +0200

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,50 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const events = [
-  { name: "Year planning", date: "11-Jan" },
-  { name: "Board meeting", date: "18-Jan" },
-  { name: "Giving to the less privileged", date: "25-Jan" },
-  { name: "Prayer and fasting", date: "2-Feb" },
-  { name: "Communion service", date: "23-Feb" },
-  { name: "Night prayer", date: "21-Feb" },
-  { name: "Leadership training", date: "01-May" },
-  { name: "Marriage seminar", date: "05-Apr" },
-  { name: "Open air evangelism", date: "14-Jun" },
-  { name: "Children's conference", date: "21-Jun" },
-  { name: "Ketekelo ea mosali ea khabane", date: "1-Aug" },
-  { name: "Expedition Saturday Fun Walk from Sekubu", date: "30-Aug" },
-  { name: "Rally for missions", date: "2-Nov" },
-  { name: "Healing campaign & personal evangelism", date: "29-Nov" },
-  { name: "Small business development training", date: "03-Dec" },
-  { name: "Revival", date: "5-Dec" },
-  { name: "Children's dedication and baptism", date: "15-Dec" },
-  { name: "Prison visits", date: "ad hoc" },
-  { name: "Bible study", date: "Every Sunday" },
-  { name: "Church service", date: "Every Sunday" },
-  { name: "Intercession", date: "Every Sunday" },
-  { name: "Wednesday prayer meeting (Needs modification)", date: "TBD" },
-  { name: "Passover Conference", date: "17-Apr" },
-];
-
-const routineServices = [
-  { 
-    name: "Wednesday Prayer Meeting", 
-    schedule: "Every Wednesday, 17:00 - 18:00",
-    image: "prayer.jpg",
-    contact: "Kelebone Lekunya +266 6320 6940"
-  },
-  { 
-    videoLink: "https://www.youtube.com/embed/wFws66W_Ftc?si=0ZHiZiXZBCDN7bwY"
-  },
-  { 
-    name: "Sunday Service", 
-    schedule: "Every Sunday, 10:00 - 13:00",
-    image: "service.jpg",
-    contact: "59193208"
-  },
-];
-
 exports.index = (req, res) => {
   const today = new Date();
   const threeMonthsLater = new Date();
@@ -71,8 +27,6 @@ exports.index = (req, res) => {
     title: 'RFI Home Page',
     currentYear: new Date().getFullYear(),
     lastModified: new Date().toLocaleString(),  // ✅ FIXED: Server-side date
-    upcomingEvents,
-    routineServices,
     activePage: 'home'
   });
 };
