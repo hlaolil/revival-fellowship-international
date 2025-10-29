aed3a007627a49b4c18e46f30038ee2ac6fd5055
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-10-29 13:29:29 +0200

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,26 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.index = (req, res) => {
-  const today = new Date();
-  const threeMonthsLater = new Date();
-  threeMonthsLater.setMonth(today.getMonth() + 3);
 
-  function parseEventDate(eventDate) {
-    const [day, month] = eventDate.split("-");
-    const year = today.getFullYear();
-    return new Date(`${month} ${day}, ${year}`);
-  }
-
-  const upcomingEvents = events
-    .filter(event => {
-      if (event.date === "ad hoc" || event.date === "Every Sunday" || event.date === "TBD") {
-        return false;
-      }
-      const eventDate = parseEventDate(event.date);
-      return eventDate >= today && eventDate <= threeMonthsLater;
-    })
-    .sort((a, b) => parseEventDate(a.date) - parseEventDate(b.date));
 
   res.render('index', {
     title: 'RFI Home Page',
